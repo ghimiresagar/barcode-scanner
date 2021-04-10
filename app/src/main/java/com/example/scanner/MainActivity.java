032ddd8580de89ea79f6d5233387022050a61c4c
ghimiresagar <sagar.ghimire@visiblebody.com>
2021-04-09 23:26:18 -0400

    added splash screen, made a new layout

--- app/src/main/java/com/example/scanner/MainActivity.java
+++ app/src/main/java/com/example/scanner/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.scanner;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // hide the initial heading bar
+        getSupportActionBar().hide();
+        // create a new handler to handle post delayed for splash screen
+        Handler handler = new Handler();
+        // delay the screen
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent toHome = new Intent(MainActivity.this, Home.class);
+                MainActivity.this.startActivity(toHome);
+                MainActivity.this.finish();
+            }
+        }, 3000);
     }
+
 }
